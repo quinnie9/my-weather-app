9a90e69ca0a4e0547355adec8d4121fa51e72bf6
QuinnVilla <quinnievillarreal@gmail.com>
2025-06-04 13:37:35 +0300

    Updated structure to have forecast vertical

--- src/script.js
+++ src/script.js
@@ -108,7 +108,7 @@ function displayForecast(response) {
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º - </strong>
           </div>
           <div class="weather-forecast-temperature">${Math.round(
             day.temperature.minimum
